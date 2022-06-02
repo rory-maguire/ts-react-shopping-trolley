abecd8dd9deb3452c77f456d5a765b046bd7cdb6
Rory Maguire <rorymaguire00@gmail.com>
2022-06-02 13:40:26 +0100

    import bootstrap css

--- ts-react-shopping-trolley/src/index.tsx
+++ ts-react-shopping-trolley/src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
 
